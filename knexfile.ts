6824e75ef223c9e3d5f848a1ed5ebc462121bcdc
Calvin <calvinalvito19@gmail.com>
2024-06-27 00:18:39 +0700

    [Update] Knex

--- knexfile.ts
+++ knexfile.ts
@@ -7,10 +7,10 @@ const knexConfig: { [key: string]: Knex.Config } = {
   development: {
     client: 'pg',
     connection: {
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
+      host: process.env.PGHOST,
+      user: process.env.PGUSER,
+      password: process.env.PGPASSWORD,
+      database: process.env.PGDATABASE,
     },
     migrations: {
       directory: './db/migrations',
