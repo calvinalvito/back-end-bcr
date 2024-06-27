84078ef7bcf58b88fb10106c4855747a1390343d
Calvin <calvinalvito19@gmail.com>
2024-06-27 17:13:50 +0700

    [Update] README, seed

--- db/seeds/users.ts
+++ db/seeds/users.ts
@@ -2,16 +2,9 @@ import { Knex } from "knex";
 
 export async function seed(knex: Knex): Promise<void> {
     // Deletes ALL existing entries
-    await knex("users").del();
+    await knex("users").where('id', 1).del();
 
     // Inserts seed entries
     await knex("users").insert([
-        { 
-        id: 1, 
-        username:"superadmin",
-        password:"$2a$12$drXXhhAyl14uLeQcAYEy/e00yL94RnwFKMuraIX/Ym2MBVcdWciu6",
-        email:"superadmin@gmail.com",
-        role:"superadmin"
-        }
     ]);
 };
